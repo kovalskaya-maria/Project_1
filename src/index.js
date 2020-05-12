76f4a321fa930ece0cec3a0f0340f65b4fb86f6f
kovalskaya-maria <kovalskaya-maria>
2020-05-12 13:57:38 +1000

    f

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let addCounter = -1;
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+
+  let ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'addition.json');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      let ourData = JSON.parse(ourRequest.responseText);
+      //renderHTML(ourData);
+      btn.insertAdjacentHTML('beforebegin', ourData[addCounter].content);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+  };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+
+  ourRequest.send();
+  addCounter++;
+  if (addCounter >= 1) {
+    btn.classList.add("hide");
+  }
+});
